4eb82bafa577a023173d2ea4575999616b84754a
JCSteiner <59900699+JCSteiner@users.noreply.github.com>
2020-09-01 13:43:21 -0400

    Add files via upload

---
+++ Transformer Raw Python/Main.py
@@ -0,0 +1,46 @@
+#%%########################## IMPORT DEPENDENCIES #############################
+
+#Imports the ability to work with advanced matrix functions
+import numpy as np
+
+# Imports our transformer model. Transformers are sequence to sequence
+# operations that take in a sequence and output a sequence in return
+from Transformer import Transformer
+
+#%%############################ DEFINE CONSTANTS ##############################
+
+# Our first constant is EMBEDDING_DIMS this is how many dimensions we transform
+# our inputs into
+EMBEDDING_DIMS = 2
+
+# Our next constant is VOCAB_SIZE, this is how many possible inputs we could
+# have, and how many outputs we will transform into
+VOCAB_SIZE = 4
+
+# Our third constant is NUM_HEADS, this is how many heads (concurrent sets of
+# weights) our self-attention mechanism will have
+NUM_HEADS = 3
+
+# Our fourth constant is NUM_BLOCKS, this is how many transformer blocks we
+# will have. This will help us calculate more complex relationships between
+# sets of sequences just like multiple layers would in any other type of
+# neural network.
+NUM_BLOCKS = 2
+
+#%%#######################
+np.random.seed(1)
+x = [0, 1]
+targets = 3
+model = Transformer(EMBEDDING_DIMS, VOCAB_SIZE, NUM_HEADS, NUM_BLOCKS)
+pred = model.fwdPass(x)
+print("Predicted:", np.argmax(pred), "Actual:", targets)
+print("Training...")
+for _ in range(10):
+    model.zeroGrad()
+    cost = 0
+    pred = model.fwdPass(x)
+    cost += model.bkwdPass(pred, targets)
+    model.step(0.1, 1)
+    print("Cost:", cost)
+pred = model.fwdPass(x)
+print("Predicted:", np.argmax(pred), "Actual:", targets)
--- Transformer Raw Python/Transformer.py
+++ Transformer Raw Python/Transformer.py
@@ -27,6 +27,7 @@ class Transformer():
         #                    model. Having many blocks can help calculate more
         #                    complex relationships between inputs
 
+        #Stores the parameters of our network as instance variables
         self.dims      = dims
         self.vocabSize = vocabSize
         self.numBlocks = numBlocks
@@ -35,7 +36,7 @@ class Transformer():
         # Initializes a matrix for our initial input embeddings, this will
         # transform from the dimensionality of the vocab to the dimensionality
         # of our transformer
-        self.embeddingMat = np.random.uniform( -0.5, 0.5, (vocabSize, dims) )
+        self.embeddingMat = np.random.uniform( 0, 1, (vocabSize, dims) )
 
         # Self Attention:
         # Self-attention mechanisms have 3 ways to weight the input.
@@ -79,13 +80,30 @@ class Transformer():
 
         self.outputWeights = np.random.uniform(-1, 1, ( vocabSize, dims ) )
 
-        self.loss = 0.0
-
+        #Initializes and zeros out our gradients
         self.zeroGrad()
 
     def zeroGrad(self):
 
         self.loss = 0.0
+
+        # Initializes our gradients
+        self.dL_dZ              = np.zeros(      self.vocabSize     )
+        self.dL_dOutputWeights  = np.zeros_like( self.outputWeights )
+        self.dL_dUnifyMats      = np.zeros_like( self.unifyMats[-1] )
+
+        # Initializes lists to store the values of the gradient of many weight
+        #  matricies within the transformer block
+        self.dL_dUnifyMatsLst = [ np.zeros_like(self.unifyMats[-1]) ] * self.numBlocks
+        self.dL_dQueriesLst   = [ np.zeros_like(self.queryMats[-1]) ] * self.numBlocks
+        self.dL_dKeysLst      = [ np.zeros_like(self.keyMats[-1]) ] * self.numBlocks
+        self.dL_dValuesLst    = [ np.zeros_like(self.valueMats[-1]) ] * self.numBlocks
+
+    #Here we define public lists that we will append to. They are needed for the learning algorithm we employ.
+    #If this was in a class structure, they would be instance variables
+
+    def fwdPass(self, inSeq):
+        # Param: inSeq - the sequence of labels we will pass into the transformer
 
         # stores the values of queries, keys, and values for each block
         self.blockQueries = []
@@ -98,24 +116,6 @@ class Transformer():
         # stores the weighted output values once they are passed through the
         # unify heads layer
         self.blockOutputs = []
-
-        # Initializes our gradients
-        self.dL_dZ              = np.zeros(      self.vocabSize     )
-        self.dL_dOutputWeights  = np.zeros_like( self.outputWeights )
-        self.dL_dUnifyMats      = np.zeros_like( self.unifyMats[-1] )
-
-        # Initializes lists to store the values of the gradient of many weight
-        #  matricies within the transformer block
-        self.dL_dUnifyMatsLst = []
-        self.dL_dQueriesLst   = []
-        self.dL_dKeysLst      = []
-        self.dL_dValuesLst    = []
-
-    #Here we define public lists that we will append to. They are needed for the learning algorithm we employ.
-    #If this was in a class structure, they would be instance variables
-
-    def fwdPass(self, inSeq):
-        # Param: inSeq - the sequence of labels we will pass into the transformer
 
         #First extracts the embedded input from the x sequence
         embeddings = [ self.embeddingMat[x] for x in inSeq ]
@@ -170,7 +170,8 @@ class Transformer():
                     weights[i][j] = np.dot(q, k)
 
             #We already calculated our raw weights, now we need to normalize those weights by passing them through the softmax function
-            weights = np.exp(weights) / np.sum(np.exp(weights), axis = 1)
+            self.weights_prime = weights
+            self.weights = np.exp(weights) / np.sum(np.exp(weights), axis = 1)
             weightedValuesBlock = []
             #For each value vector
             for i in range(len(inSeq)):
@@ -180,9 +181,10 @@ class Transformer():
 
                 #sums the weights of values over j
                 for j in range(len(inSeq)):
-
-                    #We first calculate the weighted value vector
-                    weightedValue += weights[i][j] * values[j]
+                    for h in range(self.numHeads):
+                        idx = h * self.dims
+                        #We first calculate the weighted value vector
+                        weightedValue[idx:idx+self.dims] += weights[i][j] * values[i][idx:idx+self.dims]
 
                 #Appends to the weighted values list
                 weightedValuesBlock.append( weightedValue )
@@ -200,14 +202,18 @@ class Transformer():
         #stores the output vectors of the entire transformer
         modelOutputs = []
 
+        self.yFinal = np.zeros_like(self.blockOutputs[-1][-1])
+
         #Loops through each y vector in the block outputs we have just appended to
         for y in self.blockOutputs[ -1 ]:
 
-            #The intermediate activation of casting this y vector to the
-            activation = np.dot( self.outputWeights, y )
+            self.yFinal += y
 
-            #Appends to the output vector after applying the softmax activation function to it
-            modelOutputs.append( np.exp( activation ) / np.sum( np.exp( activation ) ) )
+        #The intermediate activation of casting this y vector to the
+        activation = np.dot( self.outputWeights, self.yFinal )
+
+        #Appends to the output vector after applying the softmax activation function to it
+        modelOutputs = np.exp( activation ) / np.sum( np.exp( activation ) )
 
         #TODO possible layer normalization
 
@@ -216,30 +222,20 @@ class Transformer():
 
     def bkwdPass(self, predicted, targets):
 
-        self.loss = 0.0
+        #Converts our target into a one hot vector
+        t = np.zeros( self.vocabSize )
+        t[ targets ] = 1
 
-        #For each target in our target sequence
-        for i, target in enumerate(targets):
+        #Increments loss
+        self.loss = -np.sum( t * np.log(predicted) )
 
-            #Converts our target into a one hot vector
-            t = np.zeros( self.vocabSize )
-            t[ target ] = 1
-
-            #Increments loss
-            self.loss += -np.sum( t * np.log(predicted[i]) )
-
-            #Increments the loss with respect to the outputs
-            self.dL_dZ += predicted[i] - t
+        #Increments the loss with respect to the outputs
+        self.dL_dZ = predicted - t
 
         #Loops through the length of the final output weights
         for i in range(self.vocabSize):
             for j in range(self.dims):
-                #Loops through the each vector we had a prediction for
-                for yhat in predicted:
-                    self.dL_dOutputWeights[i][j] += self.dL_dZ[i] * yhat[j]
-
-        #We want to average over all of our predicted vectors, so we still need to divide each weight by the number of predicted vectors we have
-        self.dL_dOutputWeights /= len(predicted)
+                self.dL_dOutputWeights[i][j] += self.dL_dZ[i] * self.yFinal[j]
 
         dL_dY = np.dot(self.outputWeights.transpose(), self.dL_dZ)
 
@@ -247,28 +243,23 @@ class Transformer():
 
             for i in range(self.dims):
                 for j in range(self.dims * self.numHeads):
-                    for t in range(2):
+                    for t in range(len(self.weightedValues[-block])):
                         self.dL_dUnifyMats[i][j] += dL_dY[i] * self.weightedValues[-block][t][j]
 
-            self.dL_dUnifyMats /= len(self.blockOutputs)
+            self.dL_dUnifyMats /= len(self.weightedValues[-block])
 
             self.dL_dUnifyMatsLst.append( self.dL_dUnifyMats )
 
             dL_dValueOut = np.dot(self.unifyMats[-block].transpose(), dL_dY)
 
-            self.dL_dQueries = np.zeros_like(self.blockQueries[-block][0])
-            self.dL_dKeys    = np.zeros_like(self.blockKeys[-block][0])
-            self.dL_dValues  = np.zeros_like(self.blockValues[-block][0])
+            self.dL_dQueries = np.zeros_like(self.blockQueries[-block][-1])
+            self.dL_dKeys    = np.zeros_like(self.blockKeys[-block][-1])
+            self.dL_dValues  = np.zeros_like(self.blockValues[-block][-1])
 
             for q, k, v in zip(self.blockQueries[-block], self.blockKeys[-block], self.blockValues[-block]):
                 self.dL_dQueries += k * v * dL_dValueOut
-                self.dL_dKeys    += q * v * dL_dValueOut
-                self.dL_dValues  += q * k * dL_dValueOut
-
-            #Averages out the gradients
-            self.dL_dQueries /= len(self.blockQueries[-block])
-            self.dL_dKeys  /= len(self.blockKeys[-block])
-            self.dL_dValues /= len(self.blockValues[-block])
+                self.dL_dKeys += q * v * dL_dValueOut
+                self.dL_dValues += q * k * dL_dValueOut
 
             self.dL_dQueryWeights = np.zeros_like(self.queryMats[-block])
             self.dL_dKeyWeights   = np.zeros_like(self.keyMats[-block])
@@ -281,49 +272,21 @@ class Transformer():
                         self.dL_dKeyWeights[i][j] += self.dL_dQueries[i] * t[j]
                         self.dL_dValueWeights[i][j] += self.dL_dQueries[i] * t[j]
 
-            self.dL_dQueryWeights /= len(self.blockOutputs[-1])
-            self.dL_dKeyWeights /= len(self.blockOutputs[-1])
-            self.dL_dValueWeights /= len(self.blockOutputs[-1])
-
-            # dL_dEmbeddings = np.zeros_like(embeddingMat)
-
-
-            self.dL_dUnifyMatsLst.append(self.dL_dUnifyMats)
-            self.dL_dQueriesLst.append(self.dL_dQueryWeights)
-            self.dL_dKeysLst.append(self.dL_dKeyWeights)
-            self.dL_dValuesLst.append(self.dL_dValueWeights)
-
-        learningRate = 0.1
-
-        self.dL_dUnifyMatsLst.reverse()
-        self.dL_dQueriesLst.reverse()
-        self.dL_dKeysLst.reverse()
-        self.dL_dValuesLst.reverse()
-
-        for block in range(self.numBlocks):
-
-            self.queryMats[block] -= learningRate * self.dL_dQueriesLst[block]
-            self.keyMats[block] -= learningRate * self.dL_dKeysLst[block]
-            self.valueMats[block] -= learningRate * self.dL_dValuesLst[block]
-
-            self.unifyMats[block] -= learningRate * self.dL_dUnifyMatsLst[block]
-
-        self.outputWeights -= learningRate * self.dL_dOutputWeights
+            self.dL_dUnifyMatsLst[-block] += self.dL_dUnifyMats
+            self.dL_dQueriesLst[-block] += self.dL_dQueryWeights
+            self.dL_dKeysLst[-block] += self.dL_dKeyWeights
+            self.dL_dValuesLst[-block] += self.dL_dValueWeights
 
         return self.loss
 
-np.random.seed(27)
-x       = [0, 0]
-targets = [3, 3]
-model = Transformer(2, 4, 3, 2)
-model.zeroGrad()
-pred = model.fwdPass(x)
-print("Predicted:", [np.argmax(x) for x in pred], "Actual:", targets)
-print("Training...")
-for _ in range(100):
-    model.zeroGrad()
-    pred = model.fwdPass(x)
-    if _ % 10 == 0:
-        print("Cost:", model.bkwdPass(pred, targets))
-pred = model.fwdPass(x)
-print("Predicted:", [np.argmax(x) for x in pred], "Actual:", targets)
+    def step(self, learningRate, batchSize):
+
+        for block in range(self.numBlocks):
+
+            self.queryMats[block] -= (learningRate * self.dL_dQueriesLst[block]) / batchSize
+            self.keyMats[block] -= (learningRate * self.dL_dKeysLst[block]) / batchSize
+            self.valueMats[block] -= (learningRate * self.dL_dValuesLst[block]) / batchSize
+
+            self.unifyMats[block] -= (learningRate * self.dL_dUnifyMatsLst[block]) / batchSize
+
+        self.outputWeights -= (learningRate * self.dL_dOutputWeights) / batchSize
